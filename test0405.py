5abe90d393f0f5f9cbafade7b6047dbdfaffa2a3
Insori <s2110@e-mirim.hs.kr>
2023-04-05 08:52:48 +0900

    0405 문자열 다루기 기본

---
+++ test0405.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = True
+    if(len(s) == 4 or len(s) == 6):
+        answer = True
+        for i in s:
+            if(i < chr(48) or i > chr(57)):
+                answer = False
+                break
+    else:
+        answer = False
+    return answer
+
+print(solution("a234"))
+print(solution("1234"))
